6befecfba9bff0060eeb29b52ff618da075c3944
rebeccahurd <rhhurd@gmail.com>
2016-08-08 12:12:34 -0400

    added database tables and created file structure

--- database/data.sql
+++ database/data.sql
@@ -4,6 +4,11 @@
 
 BEGIN;
 
--- INSERT statements go here
+INSERT INTO app_user (user_name, password, salt) VALUES ('gates', 'WzYbaCzcPr6PSX0W50gUWw==', 'SDJydqT014UpMj7hCx+1/aeV7HJ82PA+CxZm0BeelT75HfxhbnAH/w5AVIHoX+A6eGiyzIMPflQlLiYhQxtB8LqddoMPWIW6jsX3MrjuKZKlRdd52az4DXdV3jWzFb+CwkGjVlLZ0SE/NvNmMxoEcwKPEEnGevnkZ8T0ZJJdh/s=');
+INSERT INTO app_user (user_name, password, salt) VALUES ('jobs', '4oM/sadeaSO27uqo5BjUZg==', 'GGQJ0g4eqopsi2HTa/uvBGSQrx8+AhhsK4DvSvQy10w9FMtcUxsvVS+MOIHUii6FoCSfQR/dJnoivxyeoRsZKU6xTGA2ByWmRI/6AJosGICwxG1s+YmgbaIqii7CtBW89r9P0uKQT8tUXW5HbXy+buQiifS8BhWr9A4YYdVcUAU=');
+INSERT INTO app_user (user_name, password, salt) VALUES ('woz', '3OVAF52wRJaH34Jd84LAiw==', 'mkhoEViWKG/044kVyqGY4hnh3pU3vkCvreGdmVb81o1Jy0VcvY9/5TTbbJ8mHV1ueV2HKhVY0W22qg0avAGxsVe+8JXr1hONvig50GhDYUdVV+qZWjyYlRnT9XWQHUDEEGAPRmYmcU8U0nmDDxT1mh0L2yayfajmxqRlVYTi8Yw=');
+INSERT INTO app_user (user_name, password, salt) VALUES ('ellison', 'xdtB3l+U+XLK7L4qemdL4w==', 'L6bgOH3ffWWGWv0S5Rtudu7FtGXn/I4hYksEEmO8vjTVh74zbvsYQtC58gWO4VvopGfE1CScD3VPLMxCgeaIpWy4WJW1bicjgA5mW8AKWYoMudYSDciNrWbjxH41JgIbWtz3mXo7JTQXU7sGyqTZRw7lghbaSrW09jovEfSc63U=');
+INSERT INTO app_user (user_name, password, salt) VALUES ('kildall', 'Dy9UfeLCtUW2PrJlMVhfJw==', 'jrojMvgS+XDQq7NZqF/gXaDTzlR8/bLbLvg4q/TeerQqL/V1/8qt4WN8zQTZCttuwJ78JY4TJSgAbq50MKDD85pzLQ1XbZtWWc8nqV+IzNaKExNcPIbXY8Ol5EGkW4RY0zbv/QAJm0u9+z41NQOY4/ctm4YBSVV+aoOGMkgE7ME=');
+
 
 COMMIT;
--- database/schema.sql
+++ database/schema.sql
@@ -4,6 +4,83 @@
 
 BEGIN;
 
--- CREATE statements go here
+CREATE TABLE app_user (
+	user_name varchar(32) NOT NULL,
+	password varchar(32) NOT NULL,
+	salt varchar(256) NOT NULL,
+	user_id integer NOT NULL,
+	CONSTRAINT pk_app_user_user_id PRIMARY KEY (user_id)
+);
+
+CREATE TABLE recipe (
+	recipe_id integer NOT NULL,
+	recipe_name varchar(32) NOT NULL,
+	meal_type_id integer,
+	category_id integer,
+	instructions varchar(256),
+	CONSTRAINT pk_recipe_recipe_id PRIMARY KEY (recipe_id),
+);
+
+CREATE TABLE recipe_ingredient (
+	recipe_id integer NOT NULL,
+	ingredient_id integer NOT NULL,
+	ingredient_quantity integer,
+	CONSTRAINT pk_recipe_ingredient_recipe_id_ingredient_id PRIMARY KEY (recipe_id, ingredient_id)
+);
+
+CREATE TABLE ingredient (
+	ingredient_id integer NOT NULL,
+	ingredient_name varchar(32) NOT NULL,
+	unit_of_measurement varchar(32) NOT NULL,
+	CONSTRAINT pk_ingredient_ingredient_id PRIMARY KEY (ingredient_id)
+);
+
+CREATE TABLE ingredient_unit (
+	ingredient_id integer NOT NULL,
+	unit_id integer NOT NULL,
+	quantity decimal NOT NULL,
+	CONSTRAINT pk_ingredient_unit_ingredient_id_unit_id PRIMARY KEY (ingredient_id, unit_id)
+);
+
+CREATE TABLE unit (
+	unit_id integer NOT NULL,
+	unit_name varchar,
+	CONSTRAINT pk_unit_unit_id PRIMARY KEY (unit_id)
+);
+
+CREATE TABLE meal_plan_recipe (
+	meal_plan_id integer NOT NULL,
+	recipe_id integer NOT NULL,
+	CONSTRAINT pk_meal_plan_recipe_meal_plan_id_recipe_id PRIMARY KEY (meal_plan_id, recipe_id)
+);
+
+CREATE TABLE meal_plan (
+	meal_plan_id integer NOT NULL,
+	meal_plan_start_date date NOT NULL,
+	meal_plan_end_date date NOT NULL,
+	CONSTRAINT pk_meal_plan_meal_plan_id PRIMARY KEY (meal_plan_id)
+);
+
+CREATE TABLE recipe_category (
+	recipe_category_id integer NOT NULL,
+	recipe_category_name varchar(32) NOT NULL,
+	recipe_category_description varchar(128),
+	CONSTRAINT pk_recipe_category_recipe_category_id PRIMARY KEY (recipe_category_id)
+);
+
+CREATE TABLE meal_type (
+	meal_type_id integer NOT NULL,
+	meal_type_name varchar(32) NOT NULL,
+	CONSTRAINT pk_meal_type_meal_type_id PRIMARY KEY (meal_type_id)
+);
+
+ALTER TABLE recipe ADD FOREIGN KEY (meal_type_id) REFERENCES meal_type(meal_type_id);
+ALTER TABLE recipe ADD FOREIGN KEY (category_id) REFERENCES recipe_category(recipe_category_id);
+ALTER TABLE recipe_ingredient ADD FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id);
+ALTER TABLE recipe_ingredient ADD FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id);
+ALTER TABLE ingredient_unit ADD FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id);
+ALTER TABLE ingredient_unit ADD FOREIGN KEY (unit_id) REFERENCES unit(unit_id);
+ALTER TABLE meal_plan_recipe ADD FOREIGN KEY (meal_plan_id) REFERENCES meal_plan(meal_plan_id);
+ALTER TABLE meal_plan_recipe ADD FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id);
 
 COMMIT;
