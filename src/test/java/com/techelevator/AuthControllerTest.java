984696eccb1604da2420089e70c0d5ac49a12b9a
James Vorous <jamesvorous@Techs-MacBook-Air-14.local>
2016-08-09 10:00:22 -0400

    trans. annotatated

--- src/test/java/com/techelevator/AuthControllerTest.java
+++ src/test/java/com/techelevator/AuthControllerTest.java
@@ -2,4 +2,12 @@ package com.techelevator;
 
 public class AuthControllerTest {
 
+	public class AuthControllerTest {
+		private AuthenticationController controller;
+		
+		@Test
+		public void display_login() {
+			
+		}
+	}
 }
