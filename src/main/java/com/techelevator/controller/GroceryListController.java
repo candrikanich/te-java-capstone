e338ea12bea6a7498b711cf26237e68db74d7758
Chris Andrikanich <chris@andrikanich.com>
2016-08-18 16:07:01 -0400

    working on hooking up the grocery list

--- src/main/java/com/techelevator/controller/GroceryListController.java
+++ src/main/java/com/techelevator/controller/GroceryListController.java
@@ -1,16 +1,25 @@
 package com.techelevator.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.techelevator.model.GroceryList;
+import com.techelevator.model.Ingredient;
+import com.techelevator.model.Recipe;
+import com.techelevator.model.User;
 import com.techelevator.model.DAO.GroceryListDAO;
 
-@Transactional
 @Controller
-
+@SessionAttributes("currentUser")
 public class GroceryListController {
 
 	private GroceryListDAO groceryListDAO;
@@ -19,10 +28,13 @@ public class GroceryListController {
 	public GroceryListController(GroceryListDAO groceryListDAO) {
 		this.groceryListDAO= groceryListDAO;
 	}
-	@RequestMapping(path="/groceryList", method=RequestMethod.GET)
-	public String displayGroceryList() {
+	@RequestMapping(path="/users/{userName}/groceryList", method=RequestMethod.GET)
+	public String displayGroceryList(@RequestParam Integer mealPlanId, ModelMap model) {
+		
+		List<Ingredient> groceryList = groceryListDAO.getAllIngredientsByMealPlan(mealPlanId);
+		model.put("groceryList", groceryList);
+		
 		return "groceryList";
 	}
-	
 	
 }
